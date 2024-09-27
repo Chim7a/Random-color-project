2eef959959d90e63b250b2de6d4ee8df95362bab
chim7a <chimalawrence11@gmail.com>
2024-09-27 22:46:06 +0100

    Javascript added

--- script.js
+++ script.js
@@ -1,11 +1,35 @@
-function domContent(params) {
-  var randomColor = Math.floor(Math.random() * 16777215).toString(16);
-  const myHeader = document.getElementById("myHeader");
-  console.log(randomColor);
+function getRandomColor() {
+  const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const changeColorBtn = document.getElementById("change-color-btn");
+  const colorBox = document.getElementById("color-box");
+  changeColorBtn.addEventListener("click", getRandomColor);
+  const greetMsg = document.getElementById("greeting");
+  const currentTime = new Date();
+  const colorCode = document.getElementById("color-code");
+
+  //   Condition to to change background color when clicked.
+  if (changeColorBtn) {
+    colorBox.style.backgroundColor = `#${randomColor}`;
+    greetMsg.style.color = `#${randomColor}`;
+    changeColorBtn.style.borderColor = `#${randomColor}`;
+    colorCode.textContent = `#${randomColor.toUpperCase()}`;
+    colorCode.style.color = "white";
+  }
+
+  //   Greeting Message
+  if (currentTime.getHours() < 12) {
+    greetMsg.innerText = `<h1>Hello</h1>`;
+  } else if (currentTime.getHours() < 16 && currentTime.getHours() != 11) {
+    greetMsg.innerText = `Good Afternoon`;
+  } else {
+    greetMsg.innerText = `Good Evening`;
+  }
 }
 
+// DOMContentLoaded
+
 if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", domContent);
+  document.addEventListener("DOMContentLoaded", getRandomColor);
 } else {
-  domContent();
+  getRandomColor();
 }
