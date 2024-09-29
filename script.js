8d7b57e71279c10f023423034b746be5baaf0580
chim7a <chimalawrence11@gmail.com>
2024-09-29 12:58:06 +0100

    BG update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function getRandomColor() {
   const randomColor = Math.floor(Math.random() * 16777215).toString(16);
   const changeColorBtn = document.getElementById("change-color-btn");
-  const colorBox = document.getElementById("color-box");
   changeColorBtn.addEventListener("click", getRandomColor);
+  const colorBox = document.getElementById("color-box");
   const greetMsg = document.getElementById("greeting");
   const currentTime = new Date();
   const colorCode = document.getElementById("color-code");
@@ -19,7 +19,7 @@ function getRandomColor() {
   //   Greeting Message
   if (currentTime.getHours() < 12) {
     greetMsg.innerText = `Good Morning`;
-  } else if (currentTime.getHours() < 16 && currentTime.getHours() != 11) {
+  } else if (currentTime.getHours() <= 16 && currentTime.getHours() != 11) {
     greetMsg.innerText = `Good Afternoon`;
   } else {
     greetMsg.innerText = `Good Evening`;
